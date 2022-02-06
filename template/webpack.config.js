f9d5b09d1076d544d143fb6aec4a0b8dd225ebb7
ienaga <ienaga@tvon.jp>
2022-02-06 23:00:42 +0900

    テンプレートの設定を変更

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -7,21 +7,17 @@ const Next2DWebpackAutoLoaderPlugin = require("@next2d/webpack-auto-loader-plugi
 module.exports = {
     "mode": "development",
     "entry": "./src/index.js",
-    "watchOptions": {
-        "ignored": [
-            path.resolve(__dirname, "src/config/Config.js"),
-            path.resolve(__dirname, "src/Packages.js")
-        ]
-    },
     "output": {
         "filename": "app.js",
         "path": path.join(__dirname, "/dist")
     },
     "module": {
-        "rules": [{
-            "test": /\.(png|svg|jpg|jpeg|gif)$/i,
-            "type": "asset/inline"
-        }]
+        "rules": [
+            {
+                "test": /\.(png|svg|jpg|jpeg|gif)$/i,
+                "type": "asset/inline"
+            }
+        ]
     },
     "plugins": [
         // If you use eslint, please uncomment it.
@@ -35,7 +31,6 @@ module.exports = {
             { "directory": path.join(__dirname, "/dist") },
             { "directory": path.join(__dirname, "/mock") }
         ],
-        "watchFiles": "src/config/*.json",
         "historyApiFallback": true,
         "compress": false,
         "open": true
