cfbbcd1c2df66579f3f055e47ae1a9a1d2edf625
ienaga <ienaga@tvon.jp>
2023-06-22 21:08:34 +0900

    #12 TypeScriptでの開発環境の調整

--- template/@types/window.d.ts
+++ template/@types/window.d.ts
@@ -1,12 +1,6 @@
 import { Next2D } from "@next2d/player/dist/player/player/Next2D";
 
-// eslint-disable-next-line no-unused-vars
-declare const next2d: Next2D;
-
 declare global {
-
     // eslint-disable-next-line no-unused-vars
-    interface Window {
-        next2d: Next2D;
-    }
+    const next2d: Next2D;
 }
